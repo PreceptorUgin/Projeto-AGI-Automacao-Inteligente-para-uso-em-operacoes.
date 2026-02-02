af3c2a1db2eedb087de9dd0734b5d226b5295c98
PreceptorUgin <juliocaynaaguiar@gmail.com>
2026-02-02 20:08:43 -0300

    Fix/:Ja vai tomando

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-async function fetchLogs() {
-  const res = await fetch('/api/logs');
-  const logs = await res.json();
-
-  const container = document.querySelector('.logs-top');
-  container.innerHTML = logs.join('<br>');
-}
-
-setInterval(fetchLogs, 2000);
